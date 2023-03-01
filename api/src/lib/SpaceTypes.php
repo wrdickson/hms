6eb91aab8e0097b2402ee414405ab0dfff73d85c
Bill Dickson <trekbill@gmail.com>
2023-03-01 14:11:07 -0700

    space type edit up

--- api/src/lib/SpaceTypes.php
+++ api/src/lib/SpaceTypes.php
@@ -19,11 +19,38 @@ class SpaceTypes {
         $iArr['id'] = $obj->id;
         $iArr['title'] = $obj->title;
         $iArr['is_active'] = $obj->is_active;
-        $arr[$obj->id] = $iArr;
+        $iArr['display_order'] = $obj->display_order;
+        array_push($arr, $iArr);
     };
     $response['execute'] = $execute;
     $response['space_types'] = $arr;
     return $response;
+  }
+
+  public static function create_space_type ( $title, $is_active, $display_order ) {
+    $pdo = DataConnector::get_connection();
+    $stmt = $pdo->prepare("INSERT INTO space_types ( title, is_active, display_order ) VALUES ( :t, :ia, :do )");
+    $stmt->bindParam(':t', $title);
+    $stmt->bindParam(':ia', $is_active);
+    $stmt->bindParam(':do', $display_order);
+    return $stmt->execute();
+  }
+
+  public static function update_space_type ( $id, $title, $display_order, $is_active ) {
+    $pdo = DataConnector::get_connection();
+    $stmt = $pdo->prepare("UPDATE space_types SET title = :t, is_active = :ia, display_order = :do WHERE id = :id ");
+    $stmt->bindParam(':t', $title);
+    $stmt->bindParam(':ia', $is_active);
+    $stmt->bindParam(':do', $display_order);
+    $stmt->bindParam(':id', $id); 
+    return $stmt->execute();
+  }
+
+  public static function delete_space_type ( $id ) {
+    $pdo = DataConnector::get_connection();
+    $stmt = $pdo->prepare("DELETE FROM space_types WHERE id = :id ");
+    $stmt->bindParam(':id', $id); 
+    return $stmt->execute();
   }
 
 }
--- api/src/route_fragments/space_types.php
+++ api/src/route_fragments/space_types.php
@@ -1,7 +1,138 @@
 <?php
 
 namespace wrdickson\hms;
-
+/**
+ * GET
+ */
 $f3->route('GET /space-types', function ($f3) {
   print json_encode(SpaceTypes::get_space_types());
+});
+
+/**
+ * CREATE
+ */
+$f3->route('POST /space-type-create', function ( $f3 ) {
+  $perms = [ 'permission' => 7, 'role' => 'edit_sale_type_groups' ];
+  //  the request should have 'Jwt' property in header with user's token
+  //  this throws an error if the token doesn't work OR user doesn't have permission
+  $f3auth = F3Auth::authorize_token( $f3, $perms );
+  
+  $account = $f3auth['decoded']->account;
+  $params = json_decode($f3->get('BODY')); 
+  $response = array();
+  $response['params'] = $params;
+  $response['account'] = $account;
+  $response['create'] = false;
+
+  //  validate
+  $options = array(
+    'is_active' => array(
+      'is_0_or_1'
+    ),
+    'title' => array(
+      'is_length, 4, 24' 
+    ),
+    'display_order' => array(
+      'is_integer'
+    )
+  );
+  $test_values = array(
+    'is_active' => $params->is_active,
+    'title' => $params->title,
+    'display_order' =>$params->display_order
+  );
+  $v = new Validate($test_values, $options);
+  $validate_result = $v->validate();
+  $response['validateResult'] = $validate_result;
+
+
+  if ( $validate_result['valid'] == true ) {
+    $response['create'] = SpaceTypes::create_space_type( $params->title, $params->is_active, $params->display_order);
+  };
+
+  print json_encode($response);
+});
+
+/**
+ * UPDATE
+ */
+$f3->route('POST /space-type-update', function ( $f3 ) {
+  $perms = [ 'permission' => 7, 'role' => 'edit_space_types' ];
+  //  the request should have 'Jwt' property in header with user's token
+  //  this throws an error if the token doesn't work OR user doesn't have permission
+  $f3auth = F3Auth::authorize_token( $f3, $perms );
+  
+  $account = $f3auth['decoded']->account;
+  $params = json_decode($f3->get('BODY')); 
+  $response = array();
+  $response['params'] = $params;
+  $response['account'] = $account;
+  $response['create'] = false;
+
+  //  validate
+  $options = array(
+    'is_active' => array(
+      'is_0_or_1'
+    ),
+    'title' => array(
+      'is_length, 4, 24' 
+    ),
+    'display_order' => array(
+      'is_integer'
+    )
+  );
+  $test_values = array(
+    'is_active' => $params->is_active,
+    'title' => $params->title,
+    'display_order' =>$params->display_order
+  );
+  $v = new Validate($test_values, $options);
+  $validate_result = $v->validate();
+  $response['validateResult'] = $validate_result;
+
+
+  if ( $validate_result['valid'] == true ) {
+    $response['update'] = SpaceTypes::update_space_type($params->id, $params->title, $params->display_order, $params->is_active);
+    $response['all_space_types'] = SpaceTypes::get_space_types();
+  };
+
+  print json_encode($response);
+});
+
+/**
+ * DELETE
+ */
+$f3->route('POST /space-type-delete', function ( $f3 ) {
+  $perms = [ 'permission' => 7, 'role' => 'edit_space_types' ];
+  //  the request should have 'Jwt' property in header with user's token
+  //  this throws an error if the token doesn't work OR user doesn't have permission
+  $f3auth = F3Auth::authorize_token( $f3, $perms );
+  
+  $account = $f3auth['decoded']->account;
+  $params = json_decode($f3->get('BODY')); 
+  $response = array();
+  $response['params'] = $params;
+  $response['account'] = $account;
+  $response['create'] = false;
+
+  //  validate
+  $options = array(
+    'space_type_id' => array(
+      'is_integer'
+    )
+  );
+  $test_values = array(
+    'space_type_id' => $params->space_type_id
+  );
+  $v = new Validate($test_values, $options);
+  $validate_result = $v->validate();
+  $response['validateResult'] = $validate_result;
+
+
+  if ( $validate_result['valid'] == true ) {
+    $response['delete'] = SpaceTypes::delete_space_type( $params->space_type_id );
+    $response['all_space_types'] = SpaceTypes::get_space_types();
+  };
+
+  print json_encode($response);
 });
