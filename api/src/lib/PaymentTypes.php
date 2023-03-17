bf87b542815a5f88d9ec223d7f7eded88b301876
Bill Dickson <trekbill@gmail.com>
2023-03-16 18:57:59 -0600

    basic config crud done

--- api/src/lib/PaymentTypes.php
+++ api/src/lib/PaymentTypes.php
@@ -4,6 +4,15 @@ namespace wrdickson\hms;
 
 Class PaymentTypes {
 
+  public static function create_payment_type ( $payment_title, $is_active, $display_order ) {
+    $pdo = DataConnector::get_connection();
+    $stmt = $pdo->prepare("INSERT INTO payment_types (payment_title, is_active, display_order) VALUES (:t, :ia, :do )");
+    $stmt->bindParam(':t', $payment_title);
+    $stmt->bindParam(':ia', $is_active);
+    $stmt->bindParam(':do', $display_order);
+    return $stmt->execute();
+  }
+
   public static function get_active_payment_types(){
     $pdo = DataConnector::get_connection();
     $stmt = $pdo->prepare("SELECT * FROM payment_types WHERE is_active = 1 ORDER BY display_order");
@@ -18,6 +27,39 @@ Class PaymentTypes {
       array_push( $active_payment_types, $i );
     }
     return $active_payment_types;
+  }
+
+  public static function get_all_payment_types(){
+    $pdo = DataConnector::get_connection();
+    $stmt = $pdo->prepare("SELECT * FROM payment_types ORDER BY display_order");
+    $stmt->execute();
+    $active_payment_types = array();
+    while( $obj = $stmt->fetchObject() ){
+      $i = array();
+      $i['id'] = $obj->id;
+      $i['payment_title'] = $obj->payment_title;
+      $i['is_active'] = $obj->is_active;
+      $i['display_order'] = $obj->display_order;
+      array_push( $active_payment_types, $i );
+    }
+    return $active_payment_types;
+  }
+
+  public static function delete_payment_type ( $id ) {
+    $pdo = DataConnector::get_connection();
+    $stmt = $pdo->prepare("DELETE FROM payment_types WHERE id = :id ");
+    $stmt->bindParam(':id', $id); 
+    return $stmt->execute();
+  }
+
+  public static function update_payment_type( $id, $payment_title, $is_active, $display_order ) {
+    $pdo = DataConnector::get_connection();
+    $stmt = $pdo->prepare("UPDATE payment_types SET payment_title = :t, is_active = :ia, display_order = :do WHERE id = :id ");
+    $stmt->bindParam(':t', $payment_title);
+    $stmt->bindParam(':ia', $is_active);
+    $stmt->bindParam(':do', $display_order);
+    $stmt->bindParam(':id', $id); 
+    return $stmt->execute();
   }
 
 }
--- api/src/route_fragments/payment_types.php
+++ api/src/route_fragments/payment_types.php
@@ -8,3 +8,142 @@ $f3->route('POST /payment-types/get-active', function ( $f3 ) {
   print json_encode($response);
 });
 
+/**
+ * GET ALL PAYMENT TYPES
+ */
+$f3->route('POST /payment-types/get-all', function ( $f3 ) {
+  $response['all_payment_types'] = PaymentTypes::get_all_payment_types();
+  $response['hello'] = 'hello';
+  print json_encode($response);
+});
+
+/**
+ * CREATE
+ */
+$f3->route('POST /payment-types/create/', function ( $f3 ) {
+  $perms = [ 'permission' => 7, 'role' => 'edit_payment_types' ];
+  //  this throws an error if the token doesn't work OR user doesn't have permission
+  $f3auth = F3Auth::authorize_token( $f3, $perms );
+
+  $account = $f3auth['decoded']->account;
+  $response = array();
+  $response['account'] = $account;
+
+  $params = json_decode($f3->get('BODY'));
+  $response['params'] = $params;
+
+  //  validate
+  $options = array(
+    'payment_title' => array(
+      'is_length, 4, 24',
+    ),
+    'display_order' => array(
+      'is_integer'
+    ),
+    'is_active' => array(
+      'is_0_or_1' 
+    )
+  );
+  $test_values = array(
+    'payment_title' => $params->payment_title,
+    'display_order' => $params->display_order,
+    'is_active' => $params->is_active
+  );
+  $v = new Validate($test_values, $options);
+  $validate_result = $v->validate();
+  $response['validateResult'] = $validate_result;
+
+  if ( $validate_result['valid'] == true ) {
+    $response['create'] = PaymentTypes::create_payment_type($params->payment_title, $params->is_active, $params->display_order);
+    $response['all_payment_types'] = PaymentTypes::get_all_payment_types();
+    $response['active_payment_types'] = PaymentTypes::get_active_payment_types();
+  };
+
+  print json_encode($response);
+});
+
+/**
+ * UPDATE
+ */
+$f3->route('POST /payment-types/update/', function ( $f3 ) {
+  $perms = [ 'permission' => 7, 'role' => 'edit_payment_types' ];
+  //  this throws an error if the token doesn't work OR user doesn't have permission
+  $f3auth = F3Auth::authorize_token( $f3, $perms );
+
+  $account = $f3auth['decoded']->account;
+  $response = array();
+  $response['account'] = $account;
+
+  $params = json_decode($f3->get('BODY'));
+  $response['params'] = $params;
+
+  //  validate
+  $options = array(
+    'id' => array(
+      'is_integer'
+    ),
+    'payment_title' => array(
+      'is_length, 4, 24'
+    ),
+    'display_order' => array(
+      'is_integer'
+    ),
+    'is_active' => array(
+      'is_0_or_1' 
+    )
+  );
+  $test_values = array(
+    'id' => $params->id,
+    'payment_title' => $params->payment_title,
+    'display_order' => $params->display_order,
+    'is_active' => $params->is_active
+  );
+  $v = new Validate($test_values, $options);
+  $validate_result = $v->validate();
+  $response['validateResult'] = $validate_result;
+
+  if ( $validate_result['valid'] == true ) {
+    $response['update'] = PaymentTypes::update_payment_type($params->id, $params->payment_title, $params->is_active, $params->display_order);
+    $response['all_payment_types'] = PaymentTypes::get_all_payment_types();
+    $response['active_payment_types'] = PaymentTypes::get_active_payment_types();
+  };
+
+  print json_encode($response);
+});
+
+/**
+ * DELETE
+ */
+$f3->route('POST /payment-types/delete/', function ( $f3 ) {
+  $perms = [ 'permission' => 7, 'role' => 'edit_payment_types' ];
+  //  this throws an error if the token doesn't work OR user doesn't have permission
+  $f3auth = F3Auth::authorize_token( $f3, $perms );
+
+  $account = $f3auth['decoded']->account;
+  $response = array();
+  $response['account'] = $account;
+
+  $params = json_decode($f3->get('BODY'));
+  $response['params'] = $params;
+
+  //  validate
+  $options = array(
+    'id' => array(
+      'is_integer'
+    )
+  );
+  $test_values = array(
+    'id' => $params->id
+  );
+  $v = new Validate($test_values, $options);
+  $validate_result = $v->validate();
+  $response['validateResult'] = $validate_result;
+
+  if ( $validate_result['valid'] == true ) {
+    $response['delete'] = PaymentTypes::delete_payment_type($params->id);
+    $response['all_payment_types'] = PaymentTypes::get_all_payment_types();
+    $response['active_payment_types'] = PaymentTypes::get_active_payment_types();
+  };
+
+  print json_encode($response);
+});
