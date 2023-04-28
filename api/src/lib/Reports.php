6ea380684e4b611d3e623502cc6a5fd7e3bf0093
Bill Dickson <trekbill@gmail.com>
2023-04-28 14:11:37 -0600

    decimal.js installed

--- api/src/lib/Reports.php
+++ api/src/lib/Reports.php
@@ -2,13 +2,107 @@
 
 namespace wrdickson\hms;
 use \PDO;
+use \DATETIME;
 
 Class Reports {
+
+  public static function generate_period_sales_report( $start, $end ) {
+
+    $pdo = DataConnector::get_connection();
+    $stmt = $pdo->prepare("SELECT sale_items.id, sale_items.folio, sale_items.sale_datetime, sale_items.sale_type, sale_types.title AS sale_type_title, sale_items.sale_subtotal, sale_items.sale_tax, sale_items.sale_total, sale_items.tax_types, sale_items.tax_spread FROM sale_items INNER JOIN sale_types ON sale_items.sale_type = sale_types.id WHERE sale_items.sale_datetime >= :start AND sale_items.sale_datetime <= :end ORDER BY sale_items.sale_datetime ASC");
+
+    $stmt->bindParam(":start", $start);
+    $stmt->bindParam(":end", $end);
+    $i = $stmt->execute();
+    $all_period_sales = array();
+    while($obj = $stmt->fetch(PDO::FETCH_OBJ)) {
+      $arr = array();
+      $arr['id'] = $obj->id;
+      $arr['folio'] = $obj->folio;
+      $arr['sale_datetime'] = $obj->sale_datetime;
+      $arr['sale_type'] = $obj->sale_type;
+      $arr['sale_subtotal'] = $obj->sale_subtotal;
+      $arr['sale_tax'] = $obj->sale_tax;
+      $arr['sale_total'] = $obj->sale_total;
+      $arr['tax_types'] = json_decode($obj->tax_types);
+      $arr['tax_spread'] = json_decode($obj->tax_spread);
+      array_push($all_period_sales, $arr);
+    }
+
+    //  create an array of all tax_types and sale_types in all_period_sales array
+    $all_included_tax_types = array();
+    $all_included_sale_types = array();
+    foreach( $all_period_sales as $sale ) {
+      //  tax_types
+      foreach( $sale['tax_types'] as $tax_type ) {
+        if(!in_array($tax_type, $all_included_tax_types)){
+          array_push($all_included_tax_types, $tax_type);
+        }
+      }
+      //  sale_type
+        if(!in_array($sale['sale_type'], $all_included_sale_types)){
+          array_push($all_included_sale_types, $sale['sale_type']);
+        }
+    }
+    sort($all_included_tax_types);
+    sort($all_included_sale_types);
+    
+    $tax_totals_by_tax_type = array();
+    foreach($all_included_tax_types as $tax_type){
+      $tax_totals_by_tax_type[$tax_type] = 0;
+    }
+    //  tax spread
+    foreach($all_included_tax_types as $tax_type){
+      foreach($all_period_sales as $sale){
+        foreach($sale['tax_spread'] as $spread_obj){
+          if($spread_obj->i == $tax_type)
+            $tax_totals_by_tax_type[$tax_type] = (intval($tax_totals_by_tax_type[$tax_type] * 100) + intval($spread_obj->t * 100))/100;
+        }
+      }
+    }
+
+    // sale_subtotal, sale_tax, sale_total
+    $sale_subtotal_by_sale_type = array();
+    $sale_tax_total_by_sale_type = array();
+    $sale_total_by_sale_type = array();
+
+    foreach($all_included_sale_types as $sale_type){
+      $sale_subtotal_by_sale_type[$sale_type] = 0;
+      $sale_tax_total_by_sale_type[$sale_type] = 0;
+      $sale_total_by_sale_type[$sale_type] = 0;
+    }
+
+    foreach($all_period_sales as $sale){
+      foreach($all_included_sale_types as $sale_type){
+        if($sale['sale_type'] == $sale_type){
+          $sale_subtotal_by_sale_type[$sale_type] = (intval($sale_subtotal_by_sale_type[$sale_type] * 100) + intval($sale['sale_subtotal'] * 100))/100;
+          $sale_tax_total_by_sale_type[$sale_type] = (intval($sale_tax_total_by_sale_type[$sale_type] * 100) + intval($sale['sale_tax'] * 100))/100;
+          $sale_total_by_sale_type[$sale_type] = (intval($sale_total_by_sale_type[$sale_type] * 100) + intval($sale['sale_total'] * 100))/100;
+        }
+      }
+    }
+   
+
+    $response = array();
+
+    $response['sale_subtotal_by_sale_type'] = $sale_subtotal_by_sale_type;
+    $response['sale_tax_total_by_sale_type'] = $sale_tax_total_by_sale_type;
+    $response['sale_total_by_sale_type'] = $sale_total_by_sale_type;
+
+    $response['all_included_sale_types'] = $all_included_sale_types;
+    $response['tax_totals_by_tax_type'] = $tax_totals_by_tax_type;
+    $response['all_included_tax_types'] = $all_included_tax_types;
+    $response['all_period_sales'] = $all_period_sales;
+    $response['report_start'] = $start;
+    $response['report_end'] = $end;
+    return $response;
+  }
 
   public static function generate_shift_report( $start, $end, $account_id ) {
     $response = array();
     $pdo = DataConnector::get_connection();
     $stmt = $pdo->prepare("SELECT payments.id, payments.folio, payments.payment_type, payments.posted_by, payments.datetime_posted, payments.total, accounts.username, payment_types.payment_title, folios.customer, customers.last_name, customers.first_name FROM payments INNER JOIN folios ON payments.folio = folios.id INNER JOIN customers ON folios.customer = customers.id INNER JOIN accounts ON payments.posted_by = accounts.id INNER JOIN payment_types ON payments.payment_type = payment_types.id WHERE payments.posted_by = :pb AND ( payments.datetime_posted > :start AND payments.datetime_posted < :end) ORDER BY payments.datetime_posted ASC");
+
     $stmt->bindParam(":pb", $account_id);
     $stmt->bindParam(":start", $start);
     $stmt->bindParam(":end", $end);
@@ -54,12 +148,13 @@ Class Reports {
           $payment_type_arr['payment_type'] = $included_payment_type;
           $payment_type_arr['payment_type_title'] = $payment['payment_title'];
           //  don't do floating point math!!!!!
-          $payment_type_arr['total'] = (intval($payment_type_arr['total'] * 100 ) + intval($payment['total'] * 100))/100;
+          $payment_type_arr['total'] = ($payment_type_arr['total'] * 100 ) + ($payment['total'] * 100)/100;
           array_push($payment_type_arr['payments'], $payment);
         }
       }
       array_push($wrapper_arr, $payment_type_arr);
     }
+
     $response['report_by_payment_type'] = $wrapper_arr;
     $account = new Account($account_id);
     $response['account'] = $account->to_array();
--- api/src/route_fragments/reports.php
+++ api/src/route_fragments/reports.php
@@ -3,7 +3,31 @@
 namespace wrdickson\hms;
 use \PDO;
 use \Exception;
+/**
+ * Period Sales Report
+ */
+$f3->route('POST /reports/period-sales-report', function ( $f3 ) {
+  $perms = [ 'permission' => 3, 'role' => 'post_payment' ];
+  //  the request should have 'Jwt' property in header with user's token
+  //  this throws an error if the token doesn't work OR user doesn't have permission
+  $f3auth = F3Auth::authorize_token( $f3, $perms );
+  $response = array();
+  $params = json_decode($f3->get('BODY'), true );
+  $response['params'] = $params;
+  $start = $params['start'];
+  $end = $params['end'];
+  try {
+    $response['sales_report'] = Reports::generate_period_sales_report( $start, $end );
+  } catch (Exception $e ) {
+    throw new Exception( $e->getMessage() );
+    $response['error'] = $e->getMessage();
+  }
+  print json_encode($response);
+});
 
+/**
+ * Shift Report
+ */
 $f3->route('POST /reports/shift-report', function ( $f3 ) {
   $perms = [ 'permission' => 3, 'role' => 'post_payment' ];
   //  the request should have 'Jwt' property in header with user's token
@@ -23,7 +47,7 @@ $f3->route('POST /reports/shift-report', function ( $f3 ) {
   try {
     //$response['start'] = $start;
     //$response['end'] = $end;
-    $response['shift_report'] = Reports::generate_shift_report($start, $end, $account_id);
+    $response['shift_report'] = Reports::generate_shift_report( $start, $end, $account_id );
     $response['all_payment_types'] = PaymentTypes::get_all_payment_types();
   } catch (Exception $e ) {
     $response['error'] = $e;
